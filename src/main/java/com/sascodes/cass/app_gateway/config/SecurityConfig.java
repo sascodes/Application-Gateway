0fe0f93a226f618c503ee6b44022114afed5bf0b
Sanjay SethuMadhavan <sanjaymdvnuniversity@gmail.com>
2024-10-16 23:12:15 +0100

    Delegated authorization to resource server from gateway

--- src/main/java/com/sascodes/cass/app_gateway/config/SecurityConfig.java
+++ src/main/java/com/sascodes/cass/app_gateway/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -17,7 +19,15 @@ public class SecurityConfig {
                 .authorizeExchange(exchange -> {
                     exchange.pathMatchers("/login/**").permitAll()
                             .anyExchange().authenticated();
-                }).oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                }).oauth2Login(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
                 .build();
     }
+
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri("http://localhost:8443/auth/realms/master/protocol/openid-connect/certs").build();
+    }
+
 }
--- src/main/java/com/sascodes/cass/app_gateway/filters/JWTAuthFilter.java
+++ src/main/java/com/sascodes/cass/app_gateway/filters/JWTAuthFilter.java
@@ -2,6 +2,7 @@ package com.sascodes.cass.app_gateway.filters;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,8 @@ public class JWTAuthFilter extends AbstractGatewayFilterFactory {
     @Override
     public GatewayFilter apply(Object config) {
         return (exchange, chain) -> {
+            String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
             exchange.getRequest().mutate()
                     .header("X_APP_NAME", "task-service")
                     .build();
